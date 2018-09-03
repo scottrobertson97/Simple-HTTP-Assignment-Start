a5ac66b14766de0821add11d16cd8e366c325053
Scott Robertson <scottrobertson97@gmail.com>
2018-09-02 21:06:04 -0400

    turotial followed up to point 34

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const textHendler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHendler.getHello(request, response);
+      break;
+    case '/time':
+      textHendler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
